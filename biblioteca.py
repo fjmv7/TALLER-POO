1ea61ccd575d82848500fc6ac324bb0a445d1968
fjmv7 <fjmurcia@ucundinamarca.edu.co>
2025-04-20 21:00:29 -0500

    va a ser libreria

---
+++ biblioteca.py
@@ -0,0 +1,12 @@
+class Biblioteca:
+    def __init__(self):
+        self.libros = []
+        self.prestados = []
+        self.usuarios = []
+        self.max_libros = 100
+        self.current
+        self.current_user = None
+        self.current = 0
+        self.total_libros = 0
+        self.libros = []
+        
